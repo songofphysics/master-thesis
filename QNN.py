1a09c6b79de05fa8f175290863c8f129529b339f
iamsong03 <songofphysics@gmail.com>
2023-11-28 00:29:50 +0100

    QNN Module

---
+++ QNN.py
@@ -0,0 +1,338 @@
+import tensorrt
+import qutip as qt
+import numpy as np
+import matplotlib.pyplot as plt
+from concurrent.futures import ThreadPoolExecutor
+from qutip import Qobj, wigner
+from tqdm import tqdm
+import time
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+# Enable GPU memory growth
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+# Utility functions
+def get_vacuum_state_tf(dim):
+    vacuum_state = qt.basis(dim, 0)
+    return tf.convert_to_tensor(vacuum_state.full(), dtype=tf.complex64)
+
+def tf_annihilation(dim):
+    diag_vals = tf.math.sqrt(tf.cast(tf.range(1, dim), dtype=tf.float32))
+    diag_vals = tf.cast(diag_vals, dtype=tf.complex64)
+    return tf.linalg.diag(diag_vals, k=1)
+
+def tf_number(dim):
+    diag_vals = tf.range(0.0, dim, dtype=tf.float32)
+    diag_vals = tf.cast(diag_vals, dtype=tf.complex64)
+    return tf.linalg.diag(diag_vals)
+
+def tf_displacement_operator(dim, alpha):
+    alpha = tf.cast(alpha, dtype=tf.complex64)
+    a = tf_annihilation(dim)
+    term1 = alpha * tf.linalg.adjoint(a)
+    term2 = tf.math.conj(alpha) * a
+    D = tf.linalg.expm(term1 - term2)
+    return D
+
+def tf_displacement_encoding(dim, alpha_vec):
+    alpha_vec = tf.cast(alpha_vec, dtype=tf.complex64)
+    num = tf.shape(alpha_vec)[0]
+    a = tf_annihilation(dim)
+    term1 = tf.linalg.adjoint(a)
+    term2 = a
+    term1_batch = tf.tile(tf.expand_dims(term1, 0), [num, 1, 1])
+    term2_batch = tf.tile(tf.expand_dims(term2, 0), [num, 1, 1])
+    alpha_vec = tf.reshape(alpha_vec, [-1, 1, 1])
+    D = tf.linalg.expm(alpha_vec * term1_batch - tf.math.conj(alpha_vec) * term2_batch)
+    return D
+
+def tf_rotation_operator(dim, theta):
+    theta = tf.cast(theta, dtype=tf.complex64)
+    n = tf_number(dim)
+    R = tf.linalg.expm(-1j * theta * n)
+    return R
+
+def tf_squeezing_operator(dim, r):
+    r = tf.cast(r, dtype=tf.complex64)
+    a = tf_annihilation(dim)
+    term1 = r * tf.linalg.adjoint(a) * a
+    term2 = tf.math.conj(r) * a * tf.linalg.adjoint(a)
+    S = tf.linalg.expm(0.5 * (term1 - term2))
+    return S
+
+def tf_kerr_operator(dim, kappa):
+    kappa = tf.cast(kappa, dtype=tf.complex64)
+    n = tf_number(dim)
+    K = tf.linalg.expm(1j * kappa * n * n)
+    return K
+
+
+# TensorFlow Custom Layer for Quantum Encoding
+class QEncoder(tf.keras.layers.Layer):
+    def __init__(self, dim, vacuum_state, **kwargs):
+        super(QEncoder, self).__init__(**kwargs)
+        self.dim = dim
+        self.vacuum_state = vacuum_state
+
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+        batch_vacuum_state = tf.tile(tf.expand_dims(self.vacuum_state, axis=0), [batch_size, 1, 1])
+        batch_displacement_operators = tf_displacement_encoding(self.dim, inputs)
+        displaced_states = tf.einsum('bij,bjk->bik', batch_displacement_operators, batch_vacuum_state)
+        return displaced_states
+    
+
+# TensorFlow Custom Layer for Quantum Transformations
+class QLayer(tf.keras.layers.Layer):
+    def __init__(self, dim, **kwargs):
+        super(QLayer, self).__init__(**kwargs)
+        self.dim = dim
+        self.final_state = None
+
+    def build(self, input_shape):
+        initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.05, seed = 42)
+        self.theta_1 = self.add_weight("theta_1", shape=[1,], initializer=initializer, trainable=True)
+        self.theta_2 = self.add_weight("theta_2", shape=[1,], initializer=initializer, trainable=True)
+        self.r = self.add_weight("r", shape=[1,], initializer=initializer, trainable=True)
+        self.b = self.add_weight("b", shape=[1,], initializer=initializer, trainable=True)
+        self.kappa = self.add_weight("kappa", shape=[1,], initializer=initializer, trainable=True)
+
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+
+        # Compute operator tensors dynamically based on the current trainable variables
+        D_tensor = tf.expand_dims(tf_displacement_operator(self.dim, self.b), 0)
+        R_tensor_1 = tf.expand_dims(tf_rotation_operator(self.dim, self.theta_1), 0)
+        S_tensor = tf.expand_dims(tf_squeezing_operator(self.dim, self.r), 0)
+        R_tensor_2 = tf.expand_dims(tf_rotation_operator(self.dim, self.theta_2), 0)
+        K_tensor = tf.expand_dims(tf_kerr_operator(self.dim, self.kappa), 0)
+
+        # Tile the operator tensors for batch processing
+        D_tensor = tf.tile(D_tensor, [batch_size, 1, 1])
+        R_tensor_1 = tf.tile(R_tensor_1, [batch_size, 1, 1])
+        S_tensor = tf.tile(S_tensor, [batch_size, 1, 1])
+        R_tensor_2 = tf.tile(R_tensor_2, [batch_size, 1, 1])
+        K_tensor = tf.tile(K_tensor, [batch_size, 1, 1])
+
+        transformed_state = tf.einsum('bij,bjk->bik', R_tensor_1, inputs)
+        transformed_state = tf.einsum('bij,bjk->bik', S_tensor, transformed_state)
+        transformed_state = tf.einsum('bij,bjk->bik', R_tensor_2, transformed_state)
+        transformed_state = tf.einsum('bij,bjk->bik', D_tensor, transformed_state)
+        
+        activated_state = tf.einsum('bij,bjk->bik', K_tensor, transformed_state)
+        self.final_state = activated_state
+        return activated_state
+    
+    
+# TensorFlow Custom Layer for Quantum Decoding
+class QDecoder(tf.keras.layers.Layer):
+    def __init__(self, dim, **kwargs):
+        super(QDecoder, self).__init__(**kwargs)
+        self.dim = dim
+        self.x_operator = self.build_x_operator()
+
+    def build_x_operator(self):
+        a = tf_annihilation(self.dim)
+        x_operator = (a + tf.linalg.adjoint(a)) / 2.0
+        x_operator = tf.expand_dims(x_operator, axis=0)  # Add batch dimension
+        return x_operator
+
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+        batch_x_operator = tf.tile(self.x_operator, [batch_size, 1, 1])
+
+        # Step 1: Compute \hat{O} | \psi \rangle for each state in the batch
+        operator_applied_state = tf.einsum('bij,bjk->bik', batch_x_operator, inputs)
+
+        # Take the complex conjugate of each state and adjust dimensions
+        conj_inputs = tf.math.conj(inputs)  # Shape: (batch_size, dim, 1)
+        conj_inputs_adj = tf.transpose(conj_inputs, perm=[0, 2, 1])  # Shape: (batch_size, 1, dim)
+
+        # Compute the expectation value (inner product) for each state in the batch
+        x_expectation = tf.einsum('bij,bjk->bi', conj_inputs_adj, operator_applied_state) 
+        x_expectation = tf.squeeze(x_expectation, axis=-1)
+
+        return tf.cast(x_expectation, dtype=tf.float32)
+    
+
+# TensorFlow Custom Callback for Wigner Logarithmic Negativity    
+class WLN_Monitor(tf.keras.callbacks.Callback):
+    def __init__(self, model, last_layer_index, dim, xvec, dx, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model
+        self.last_layer_index = last_layer_index
+        self.dim = dim
+        self.xvec = xvec
+        self.dx = dx
+        self.wln = []
+
+    def compute_wln(self, state_ket_array):
+        state_ket_reshaped = state_ket_array.squeeze()
+        state_ket = Qobj(state_ket_reshaped).unit()  # Convert to Qobj
+        wigner_func = wigner(state_ket, self.xvec, self.xvec)
+        da = self.dx * self.dx
+        return np.log(np.sum(np.abs(wigner_func)*da))
+
+    def on_epoch_end(self, epoch, logs=None):
+        quantum_output = tf.keras.Model(inputs=self.model.input, 
+                                            outputs=self.model.layers[self.last_layer_index].output)
+        
+        # Predict to get the quantum states
+        quantum_states = quantum_output.predict(self.xvec, verbose = 0)
+
+        # Prepare the states for parallel processing
+        state_kets_arrays = [quantum_states[i, :, 0] for i in range(quantum_states.shape[0])]
+
+        # Use ThreadPoolExecutor to parallelize the computation
+        with ThreadPoolExecutor() as executor:
+            negativities = list(executor.map(self.compute_wln, state_kets_arrays))
+
+        # Average negativity over the batch
+        avg_negativity = np.mean(negativities)
+        self.wln.append(avg_negativity)
+
+    def get_wln(self):
+        return self.wln
+
+
+# TensorFlow Custom Layer for Classical Phase Space Transformations
+class CPLayer(tf.keras.layers.Layer):
+    def __init__(self):
+        super(CPLayer, self).__init__()
+        initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.05, seed=42)
+        
+        # Initialize the weights for the rotations, squeezing, translations, and nonlinear activation
+        self.theta1 = self.add_weight(shape=(1,), initializer=initializer, trainable=True)
+        self.theta2 = self.add_weight(shape=(1,), initializer=initializer, trainable=True)
+        self.r = self.add_weight(shape=(1,), initializer=initializer, trainable=True)
+        self.kappa = self.add_weight(shape=(1,), initializer=initializer, trainable=True)
+        self.b = self.add_weight(shape=(1,), initializer=initializer, trainable=True)
+        
+    def call(self, inputs):
+        # Unpack x and p components
+        x, p = inputs[..., 0], inputs[..., 1]
+        
+        # Apply the first rotation
+        x_rot = x * tf.cos(self.theta1) - p * tf.sin(self.theta1)
+        p_rot = x * tf.sin(self.theta1) + p * tf.cos(self.theta1)
+        
+        # Apply squeezing
+        x_squeezed = tf.exp(-self.r) * x_rot
+        p_squeezed = tf.exp(self.r) * p_rot
+        
+        # Apply the second rotation
+        x_rot2 = x_squeezed * tf.cos(self.theta2) - p_squeezed * tf.sin(self.theta2)
+        p_rot2 = x_squeezed * tf.sin(self.theta2) + p_squeezed * tf.cos(self.theta2)
+        
+        # Apply translation
+        x_translated = x_rot2 + self.b
+        p_translated = p_rot2
+        
+        # Calculate radius for the activation
+        radius = tf.sqrt(x_translated**2 + p_translated**2)
+        
+        # Apply the nonlinear activation
+        x_activated = x_translated * tf.cos(self.kappa * radius) - p_translated * tf.sin(self.kappa * radius)
+        p_activated = x_translated * tf.sin(self.kappa * radius) + p_translated * tf.cos(self.kappa * radius)
+        
+        # Stack the transformed components together to return a 2D output
+        outputs = tf.stack([x_activated, p_activated], axis=-1)
+        return outputs
+    
+    
+# TensorFlow Custom Layer for Extracting Positions
+class ExtractXLayer(tf.keras.layers.Layer):
+    def __init__(self):
+        super(ExtractXLayer, self).__init__()
+
+    def call(self, inputs):
+        # Assuming the 'x' values are the first component in the (N, 2) input
+        # Extracts and returns the 'x' component in shape (N, 1)
+        return tf.expand_dims(inputs[..., 0], axis=-1)
+    
+        
+# TensorFlow Custom Callback for Progress Bars
+class TQDMProgressBar(tf.keras.callbacks.Callback):
+    def on_train_begin(self, logs=None):
+        self.epochs = self.params['epochs']
+        self.progress_bar = tqdm(total=self.epochs, desc=f"Epoch 1/{self.epochs}")
+        self.start_time = time.time()  # Start time
+
+    def on_epoch_end(self, epoch, logs=None):
+        description = f"Epoch {epoch+1}/{self.epochs}"
+        self.progress_bar.set_description(description)
+        self.progress_bar.update(1)
+
+    def on_train_end(self, logs=None):
+        self.progress_bar.close()
+        self.end_time = time.time()  # End time
+        self.total_time = self.end_time - self.start_time  # Total computation time
+        print(f"Total training time: {self.total_time:.2f} seconds")
+
+
+# Function for training models with different configurations
+def train_models(input_data, target_data, cutoff_dim = 10, configs = [(6, 50)], qmonitor = False):
+    trained_models = []
+    histories = []
+    quantumness = []
+    
+    for num_layers, epochs in configs:
+        # Create a new model for each configuration
+        vacuum_state = get_vacuum_state_tf(cutoff_dim[0])
+        model = tf.keras.Sequential([QEncoder(dim=cutoff_dim[0], vacuum_state=vacuum_state, name='QuantumEncoding')])
+        for i in range(num_layers):
+            model.add(QLayer(dim=cutoff_dim[0], name=f'QuantumLayer_{i+1}'))
+        model.add(QDecoder(dim=cutoff_dim[0], name='QuantumDecoding'))
+
+        # Compile and train the model
+        model.compile(optimizer='adam', loss='mse')
+        print(f'Training model with {num_layers} layers for {epochs} epochs...')
+        if qmonitor == True:
+            WLN = WLN_Monitor(model, -2, dim=cutoff_dim[0], xvec=input_data, dx = input_data[0]-input_data[1])
+            history = model.fit(input_data, target_data, validation_split=0.30, epochs=epochs, verbose=0, callbacks=[WLN])
+            quantumness.append(WLN.get_wln())
+        else:
+            showprogress = TQDMProgressBar()
+            history = model.fit(input_data, target_data, validation_split=0.30, epochs=epochs, verbose=0, callbacks=[showprogress])
+        print('Training Complete.')
+        model.summary()
+
+        # Store the trained model and its history
+        histories.append(history)
+        trained_models.append(model)
+
+    return trained_models, histories, quantumness
+
+
+# Function for training classical models with different configurations
+def train_classical_models(configs, input_data, target_data):
+    trained_models = []
+    histories = []
+    for num_layers, epochs in configs:
+        # Create a new model for each configuration
+        layers = [CPLayer() for _ in range(num_layers)] + [ExtractXLayer()]
+        model = tf.keras.Sequential(layers)
+
+        # When compiling the model, specify the reduction to 'none' to avoid reducing the loss
+        model.compile(optimizer='adam', loss='mse')
+        print(f'Training model with {num_layers} layers for {epochs} epochs...')
+        progress_bar = TQDMProgressBar()
+        history = model.fit(input_data, target_data, validation_split=0.10, epochs=epochs, verbose=0, callbacks=[progress_bar])
+
+        # Store the trained model and its history
+        histories.append(history)
+        trained_models.append(model)
+
+    return trained_models, histories
